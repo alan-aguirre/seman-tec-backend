135f6a205528089de1d455e3519e5e7499efcd48
alan-aguirre <79216727+alan-aguirre@users.noreply.github.com>
2021-10-29 13:56:04 -0500

    Final

---
+++ app.py
@@ -0,0 +1,35 @@
+from flask import Flask
+from flask import request
+from flask import jsonify
+from flask_pymongo import pymongo
+from bson import json_util
+import ssl
+import json
+import uuid
+from urllib.request import urlopen
+import urllib
+
+app = Flask(__name__)
+
+CONNECTION_STRING = "mongodb+srv://inigo:prueba@cluster0.0lpx0.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+client = pymongo.MongoClient(CONNECTION_STRING, ssl_cert_reqs=ssl.CERT_NONE)
+db = client.get_database('flask_mongodb_atlas')
+covid_collection = pymongo.collection.Collection(db, 'covid_collection')
+
+@app.route("/cuestionario", methods=['POST'])
+def create_cuestionario():
+    data=request.json
+    covid_collection.insert_one({"_id":str(uuid.uuid4()),"covid":data["covid"],"profesion":data["profesion"], "vacunado":data["vacunado"]})
+    return "Cuestionario completado"
+
+@app.route("/cuestionario/list",methods=['GET'])
+def list_cuestionario():
+    cuestionario = covid_collection.find()
+    response=[todo for todo in cuestionario]
+    return json.dumps(response, default=json_util.default)
+
+#@app.route("/")
+#def test():
+#    covid_collection.insert_one({"name": "John"})
+#    return "Connected to the dataabase!"
+
